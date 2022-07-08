6cd2b5480f86934df3c05c3204969cac96dd35e3
Kuldeepsinh Jhala <kuldeephjhala@gmail.com>
2022-07-08 23:54:43 +0530

    day 4

--- customer.php
+++ customer.php
@@ -6,14 +6,20 @@
 
     <title>Customer List</title>
 
+    <!-- bootstrap CDN -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 
     <!-- STYLE -->
     <link rel="stylesheet" href="css\styles.css?v=<?php echo time(); ?>">
 
+    <!-- FONT-AWESOME -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
   </head>
-  <body>
+  <body class="customer-body">
 
     <div class="customer-title">
       Bank Customer
@@ -23,7 +29,7 @@
     <div class="row">
         <div class="col-md-12">
 
-            <table class="table">
+            <table class="table table-hover table-dark table-curved">
               <thead>
                 <tr>
                   <th scope="col">id</th>
@@ -31,6 +37,7 @@
                   <th scope="col">Account No</th>
                   <th scope="col">Current Balance</th>
                   <th scope="col">Email</th>
+                  <th scope="col" class="transaction-button-heading">Transfer Balance</th>
                 </tr>
               </thead>
               <tbody>
@@ -46,6 +53,7 @@
                     <td><?php echo $array[2];?></td>
                     <td><?php echo $array[3];?></td>
                     <td><?php echo $array[4];?></td>
+                    <td align="center"><a href="perform-transaction.php?id=<?php echo $array[0];?>"> <button type="button" class="btn btn-light transaction-button"><i class="fa-solid fa-dollar-sign"></i> Transfer Money</button> </a> </td>
                 </tr>
 
                 <?php endwhile; ?>
---
+++ perform-transaction.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+      $from = $_GET['id'];
+      echo $from;
+    ?>
+  </body>
+</html>
